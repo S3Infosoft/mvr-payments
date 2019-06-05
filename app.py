b3f297d111d663e1cefe20d179fb978381b58a68
itsmanjeet <sainishaab550@gmail.com>
2019-06-05 15:09:01 +0530

    Divide app.py into Init.py Modules.py and App.py for easy Configurations

--- app.py
+++ app.py
@@ -1,31 +1,9 @@
 #!/usr/bin/python3
-from flask import Flask
+from flask import request
 import json
-from flask_sqlalchemy import SQLAlchemy
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/data.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+from Init import db, app
+from Models import Reservations
 
-db = SQLAlchemy(app)
-
-class Reservations(db.Model):
-    ID = db.Column('ID', db.Integer, primary_key = True)
-    RDATE = db.Column('RESERVATION DATE', db.String)
-    RFROM = db.Column('RESERVATION FROM', db.String(100))
-    CKINDATE = db.Column('CHECKIN DATE', db.String)
-    CKOUTDATE = db.Column('CHECKOUT DATE',db.String)
-    GNAME = db.Column('GUEST NAME', db.String(100))
-    GEMAIL = db.Column('GUEST EMAIL', db.String(100))
-    GMOBNUM = db.Column('GUEST CONTACT NUMBER', db.Integer)
-    BOOKAMNT = db.Column('BOOKING AMOUNT', db.Integer)
-    COMAMNT = db.Column('COMMISSION AMOUNT', db.Integer)
-    COMTAX = db.Column('COMMISSION TAX', db.Integer)
-    TOTCOM = db.Column('TOTAL COMMISSION', db.Integer)
-    AMNTREC = db.Column('AMOUNT RECEIVABLE', db.Integer)
-    CMNT = db.Column('COMMENTS', db.String(500))
-
-db.init_app(app)
-db.create_all()
 @app.route('/')
 def Fetch():
     USERS = Reservations.query.all()
@@ -50,6 +28,11 @@ def Fetch():
         ALL.append(new)
     
     return json.dumps(ALL), 200
+
+@app.route('/Create', methods=['POST'])
+def add():
+    data = request.get_json()
+    name = data['']
 
 if __name__ == '__main__':
     
