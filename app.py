41479908f29f9a93d6633b15a360dba378307551
itsmanjeet <sainishaab550@gmail.com>
2019-06-06 13:09:49 +0530

    add selection method with reservation date

--- app.py
+++ app.py
@@ -1,30 +1,7 @@
 #!/usr/bin/python3
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from init import app, db
+from Model import Reservations
 import json
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/database.db'
-
-db = SQLAlchemy(app)
-
-class Reservations(db.Model):
-    ID = db.Column('ID',db.Integer,primary_key = True)
-    RDATE = db.Column('RESERVATION DATE', db.String)
-    RFROM = db.Column('RESERVATION FROM', db.String(100))
-    CKINDATE = db.Column('CHECKIN DATE', db.String)
-    CKOUTDATE = db.Column('CHECKOUT DATE',db.String)
-    GNAME = db.Column('GUEST NAME', db.String(100))
-    GEMAIL = db.Column('GUEST EMAIL', db.String(100))
-    GMOBNUM = db.Column('GUEST CONTACT NUMBER', db.Integer)
-    BOOKAMNT = db.Column('BOOKING AMOUNT', db.Integer)
-    COMAMNT = db.Column('COMMISSION AMOUNT', db.Integer)
-    COMTAX = db.Column('COMMISSION TAX', db.Integer)
-    TOTCOM = db.Column('TOTAL COMMISSION', db.Integer)
-    AMNTREC = db.Column('AMOUNT RECEIVABLE', db.Integer)
-    CMNT = db.Column('COMMENTS', db.String(500))
-
-db.create_all()
 
 @app.route('/')
 def All():
@@ -61,6 +38,9 @@ def Select(method,data):
         data = str(data)
         data = data.replace('_',' ')
         selected_users = Reservations.query.filter_by(GNAME = data).all()
+    elif (method == 'rdate'):
+        data = data.replace('_','/')
+        selected_users = Reservations.query.filter_by(RDATE = data).all()
     else:
         return 'Unknow Method'
     all_selected = []
@@ -87,4 +67,4 @@ def Select(method,data):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
