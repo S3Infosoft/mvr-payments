35932d327d6f2fab73afe7aae26c6e78124f3642
itsmanjeet <sainishaab550@gmail.com>
2019-06-07 17:03:08 +0530

    Update Method for Secret Key Generation

--- init.py
+++ init.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
+
+secret_key = os.urandom(12).hex()
 
 app = Flask(__name__)
-app.secret_key = '123456789'
+app.secret_key = secret_key
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/database.db'
 
 db = SQLAlchemy(app)
