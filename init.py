86d8576e912646fad3ca7c7bf48874fcf838b77c
itsmanjeet <sainishaab550@gmail.com>
2019-06-07 10:42:05 +0530

    Added secret key (For Post Method)

--- init.py
+++ init.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = '123456789'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/database.db'
 
 db = SQLAlchemy(app)
