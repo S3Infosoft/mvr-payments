af6815a8c28757e2be0f31872be4e2e4d3d500a6
itsmanjeet <sainishaab550@gmail.com>
2019-06-07 10:41:23 +0530

    Added Create and Delete Functions

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from init import app, db
 from Model import Reservations
 import json
-
+from flask import request, jsonify, flash, url_for,redirect
 @app.route('/')
 def All():
     users = Reservations.query.all()
@@ -65,6 +65,39 @@ def Select(method,data):
 
     return json.dumps(all_selected)
 
+@app.route('/v1/Create/', methods = ['POST'])
+def Create():
+    print(request.is_json)
+    data = request.get_json()
+    print(data)
+    user = Reservations(data['Reservation Date'],
+                        data['Reservation From'],
+                        data['Check in Date'],
+                        data['Check Out Date'],
+                        data['Guest Name'],
+                        data['Guest Email'],
+                        data['Guest Contact No.'],
+                        data['Booking Amount'],
+                        data['Commission Amount'],
+                        data['Commission Tax'],
+                        data['Total Commission'],
+                        data['Amount Recivable'],
+                        data['Comment']
+           )
+    db.session.add(user)
+    db.session.commit()
+
+    flash('data added successfully')
+    
+    return redirect(url_for('All'))
+
+@app.route('/v1/Delete/<uid>')
+def Delete(uid):
+    user_to_delete = Reservations.query.filter_by(ID = uid).all()[0]
+    db.session.delete(user_to_delete)
+    db.session.commit()
+    flash('data for ID %s Deleted Successfully'%uid)
+    return redirect(url_for('All'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0')
