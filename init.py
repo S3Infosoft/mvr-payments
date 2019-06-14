c6faeabf51adc0f7f9184a977a556419f9e3d0ec
itsmanjeet <sainishaab550@gmail.com>
2019-06-14 13:32:14 +0530

    fixed init.py

--- init.py
+++ init.py
@@ -1,11 +1,8 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 import os
 
 secret_key = os.urandom(12).hex()
 
 app = Flask(__name__)
 app.secret_key = secret_key
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/database.db'
-
-db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/database.db'
