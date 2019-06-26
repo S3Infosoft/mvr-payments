a433a415319e03927e8d1717a633be0510d6e33b
itsmanjeet <sainishaab550@gmail.com>
2019-06-26 16:19:54 +0530

    __ no need fix (only to hide some warning)

--- app/init.py
+++ app/init.py
@@ -6,3 +6,4 @@ from config import __secret_key__
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
 app.secret_key = __secret_key__
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../database/database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
