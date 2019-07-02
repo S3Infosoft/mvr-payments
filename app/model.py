eba70ba8bad9874cbeb78c501c2a3c0c6f66eaa6
Manjeet Saini <sainishaab550@gmail.com>
2019-07-02 23:09:44 +0530

    Removed extra functions from User Model not required by jwt

--- app/model.py
+++ app/model.py
@@ -126,29 +126,12 @@ class User(database.Model):
         self.email = email
         self.password = self.__encrypt__(password)
 
-    def get_auth_token(self):
-        token = token_serializer.dumps({'email':self.email,
-                                        'password':self.password}).decode('utf-8')
-        return token
-
     def __encrypt__(self,password):
         h = hashlib.md5(password.encode())
         return h.hexdigest()
 
     def verify(self,password):
         return self.password == self.__encrypt__(password)
-    
-    def is_active(self):
-        return True
-
-    def is_authenticated(self):
-        return True
-
-    def get_id(self):
-        return self.email
-
-    def is_anomymous(self):
-        return False
 
 
 database.create_all()
