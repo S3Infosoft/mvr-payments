405eb26595bd6129ba9715f3f7cd93aa4ebeeca3
itsmanjeet <sainishaab550@gmail.com>
2019-07-03 22:40:49 +0530

    Renamed route to api

--- app/api.py
+++ app/api.py
@@ -5,11 +5,15 @@ from flask import json
 from app.routes.login import login_api, login_required
 from app.routes.guest import guest_api
 from app.routes.reservation import reservation_api
+from app.routes.transaction import transaction_api
+
 from app.auth import jwt_required, fresh_jwt_required
+
 
 app.register_blueprint(login_api)
 app.register_blueprint(guest_api)
 app.register_blueprint(reservation_api)
+app.register_blueprint(transaction_api)
 
 @app.route('/')
 @login_required
